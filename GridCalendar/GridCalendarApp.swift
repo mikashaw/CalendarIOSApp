4f879ad7c8f7ba599b1250dd478780ffb829e291
mika-shaw <66797474+mikashaw@users.noreply.github.com>
2022-06-24 16:20:39 -0400

    Add files via upload

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Mika Shaw on 2022-06-23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
